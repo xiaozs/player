196613794b074a15f7938e91ff3203973844cc37
unknown <xiaozhensheng@gosuncn.com>
2019-05-07 17:10:17 +0800

    decoder.ts 修改

--- src/decoder/Decoder.ts
+++ src/decoder/Decoder.ts
@@ -2,83 +2,69 @@ import { EventEmitter } from "../utils/EventEmitter";
 import { PlayerParts } from "../utils/PlayerParts";
 import { listen } from "../utils/listen";
 
-class WorkerProxy extends EventEmitter {
-    private worker!: Worker;
-    constructor(workerUrl: string) {
-        super();
-        this.initProxy(workerUrl);
+class DecoderProxy {
+    private static decoderId = 0;
+    private decoderId: number;
+    constructor(private worker: Worker, fileName: string) {
+        this.decoderId = DecoderProxy.decoderId++;
+        this.openDecoder(fileName);
     }
-    private post(type: string, transfer?: Transferable) {
-        let data = {
-            type: type,
-            data: transfer || null
-        };
-        this.worker.postMessage(data, transfer && [transfer]);
-    }
-    private initProxy(workerUrl: string) {
-        this.worker = new Worker(workerUrl)
-        this.worker.addEventListener("message", e => {
-            let data = e.data;
-            let type = data.type;
-            this.trigger(type, data);
-        });
-        this.init();
-    }
-    init() {
-        this.post("init");
-    }
-    uninit() {
-        return new Promise(resolve => {
-            let callback = (data: any) => {
-                if (data.type === "uninit") {
-                    resolve()
-                    this.worker.removeEventListener("message", callback);
-                }
-            };
-            this.worker.addEventListener("message", callback)
-            this.post("uninit");
+    private openDecoder(fileName: string) {
+        this.worker.postMessage({
+            type: "openDecoder",
+            data: {
+                decoderId: this.decoderId,
+                fileName
+            }
         })
     }
-    open() {
-        this.post("open");
-    }
-    close() {
-        this.post("close");
-    }
-    decode(chunk: ArrayBuffer) {
-        this.post("decode", chunk);
-    }
-    async destroy() {
-        this.close();
-        this.uninit();
-        await this.off();
-        this.worker.terminate();
+    destroy() {
+        this.worker.postMessage({
+            type: "closeDecoder",
+            data: {
+                decoderId: this.decoderId
+            }
+        })
     }
 }
 
 export class Decoder extends PlayerParts {
-    private worker!: WorkerProxy;
+    private worker!: Worker;
+    private decoderProxys: DecoderProxy[] = [];
     constructor(workerUrl: string, eventBus: EventEmitter) {
         super(eventBus);
         this.initWorker(workerUrl);
+
+        //todo,先写死一路，写死类型
+        this.decoderProxys.push(new DecoderProxy(this.worker, "mp4"));
     }
 
     private initWorker(workerUrl: string) {
-        //todo, 这个worker肯定要大改的
-        this.worker = new WorkerProxy(workerUrl);
-        this.worker.on("decoder-videoFrame", (data: any) => this.trigger("decoder-videoFrame", data));
-        this.worker.on("decoder-audioFrame", (data: any) => this.trigger("decoder-audioFrame", data));
+        this.worker = new Worker(workerUrl);
+        this.worker.addEventListener("message", e => {
+            let eData = e.data;
+            let type = eData.type;
+            let data = eData.data;
+            this.trigger(type, data);
+        });
     }
 
     @listen("destroy")
     private onDestory(): void {
         this.off();
-        this.worker.destroy();
+        this.decoderProxys.forEach(it => it.destroy());
+        this.worker.postMessage({ type: "uninitDecoder" });
     }
 
     @listen("loader-chunked")
     private onChunked(chunk: ArrayBuffer) {
-        console.log(chunk.byteLength);
-        this.worker.decode(chunk);
+        this.worker.postMessage({
+            type: "inputData",
+            data: {
+                //todo,先写死一路
+                decoderId: 0,
+                data: chunk
+            }
+        }, [chunk]);
     }
 }
