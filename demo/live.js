c224e57515d90955000653c986560b6d44527c34
unknown <xiaozhensheng@gosuncn.com>
2019-07-23 17:48:32 +0800

    修改demo

---
+++ demo/live.js
@@ -0,0 +1,46 @@
+
+var template = $("#template").html();
+var $container = $("#container");
+
+$("#add").click(function () {
+    var $new = $(template);
+    $new.on("click", ".play", onPlay)
+        .on("click", ".pause", onPause)
+        .on("click", ".fullscreen", onFullscreen);
+    $container.append($new);
+})
+
+function getPlayer($item) {
+    var player = $item.data("player");
+    if (!player) {
+        var url = $.trim($item.find(".input").val());
+        var canvas = $item.find(".canvas")[0];
+        var player = new GoVideo.Player({
+            canvas: canvas,
+            url: url,
+            fileName: url.replace(/.*\.(.*)$/, "$1"),
+            loaderType: "live",
+            workerUrl: "./dist/decodeWorker.js"
+        });
+        $item.data("player", player);
+    }
+    return player;
+}
+
+function onPlay() {
+    var $item = $(this).parents(".item");
+    var player = getPlayer($item);
+    player.play();
+}
+
+function onPause() {
+    var $item = $(this).parents(".item");
+    var player = getPlayer($item);
+    player.pause();
+}
+
+function onFullscreen() {
+    var $item = $(this).parents(".item");
+    var canvas = $item.find(".canvas")[0];
+    canvas.requestFullscreen();
+}
